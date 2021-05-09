72e069df8574b755eb80dc9b97481c41dad2d339
averkova-a <averkon8@gmail.com@>
2021-05-09 12:10:37 +0300

    refactor book adding

--- app/src/main/java/ua/kpi/comsys/io8371/ui/book/BookFragment.kt
+++ app/src/main/java/ua/kpi/comsys/io8371/ui/book/BookFragment.kt
@@ -10,7 +10,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.ItemTouchHelper
-import com.example.flatdialoglibrary.dialog.FlatDialog
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -70,28 +70,33 @@ class BookFragment : Fragment() {
         view.findViewById<RecyclerView>(R.id.list).adapter = adapter
         enableSwipeToDeleteAndUndo()
         addButton = requireView().findViewById(R.id.add)
-        addButton.setOnClickListener {
-            val flatDialog = FlatDialog(requireContext())
-            flatDialog.setTitle("New book")
-                    .setFirstTextFieldHint("Title")
-                    .setSecondTextFieldHint("Price")
-                    .setLargeTextFieldHint("Description")
-                    .setFirstButtonText("Add")
-                    .setSecondButtonText("Cancel")
-                    .withFirstButtonListner {
-                        val search = Book()
-                        search.title = flatDialog.firstTextField
-                        search.subtitle = flatDialog.secondTextField
-                        search.price=flatDialog.largeTextField
-                        bookContainer.search.add(search)
-                        flatDialog.dismiss()
-                    }
-                    .withSecondButtonListner {
-                        flatDialog.dismiss()
-                    }
-                    .show()
+        addButton.setOnClickListener { withEditText(addButton)
         }
     }
+
+    fun withEditText(view: View) {
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = layoutInflater
+        builder.setTitle("Add new book")
+        val dialogLayout = inflater.inflate(R.layout.alert_dialog_with_edittext, null)
+        val title = dialogLayout.findViewById<EditText>(R.id.title)
+        val subtitle = dialogLayout.findViewById<EditText>(R.id.subtitle)
+        var price = dialogLayout.findViewById<EditText>(R.id.price)
+        builder.setView(dialogLayout)
+        builder.setPositiveButton("Add") { dialogInterface, i ->
+            run {
+                val search = Book()
+                search.title = title.text.toString()
+                search.subtitle = subtitle.text.toString()
+                search.price = price.text.toString()
+                bookContainer.search.add(search)
+                dialogInterface.cancel()
+            }
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
+    }
+
 
     private fun initBook() {
         val mapper = ObjectMapper()
